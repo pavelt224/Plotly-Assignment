c69ededb4ffbe8541851cdd238b502064ccf2a13
Pavel Tenenbaum <93852380+pavelt224@users.noreply.github.com>
2022-02-06 16:13:50 -0500

    Up2

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data.sample.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -27,12 +27,12 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
-  
+
 }
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data.sample.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +56,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("./static/data.sample.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -71,17 +71,17 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    // var yticks = 
 
     // 8. Create the trace for the bar chart. 
     var barData = [
-      
+
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+
   });
 }
